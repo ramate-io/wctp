dadf71bd5e33c1b15b186c8de7e3a3345ad18abb
Liam Makena Monninger <liam@ramate.io>
2025-11-26 11:55:18 +0100

    chore: playing with union cases.

--- playgrounds/terrain/src/sdf/analysis/interval/combinators/union/boundary.rs
+++ playgrounds/terrain/src/sdf/analysis/interval/combinators/union/boundary.rs
@@ -16,8 +16,13 @@ impl SignBoundary {
 				}
 			}
 			Sign::Positive => {
-				// whatever the sign on self is at the max of it and other
-				vec![SignBoundary { min: self.min.max(other.min), sign: self.sign.clone() }]
+				// You can just keep self.
+				// If self is positive, then that positivity is preserved.
+				// If self is negative, then that negativity is preserved--valid union.
+				// If self is top, then topness is preserved. The previous interval would can
+				// take whatever the value up to point.
+				// Bottom is the same logic as top.
+				vec![self.clone()]
 			}
 			Sign::Top => {
 				// This is unknown from the lowest point
@@ -99,6 +104,106 @@ mod tests {
 				SignBoundary { min: 0.0, sign: Sign::Positive },
 				SignBoundary { min: 1.0, sign: Sign::Negative },
 				SignBoundary { min: 2.0, sign: Sign::Positive },
+			]
+		)
+	}
+
+	#[test]
+	fn test_unions_over_rhs_many_complex() {
+		let boundary = SignBoundary { min: 2.0, sign: Sign::Negative };
+		let others = vec![
+			SignBoundary { min: 1.0, sign: Sign::Negative },
+			SignBoundary { min: 3.0, sign: Sign::Positive },
+		];
+		let result = boundary.unions_over(&others);
+		assert_eq!(
+			result,
+			vec![
+				// normalization will later combine these two
+				SignBoundary { min: 1.0, sign: Sign::Negative },
+				SignBoundary { min: 2.0, sign: Sign::Negative },
+			]
+		)
+	}
+
+	#[test]
+	fn test_unions_on_simple() {
+		let mapping = vec![
+			(
+				SignBoundary { min: 0.0, sign: Sign::Positive },
+				vec![SignBoundary { min: 1.0, sign: Sign::Negative }],
+			),
+			(
+				SignBoundary { min: 2.0, sign: Sign::Negative },
+				vec![
+					// because the RHS boundary from 1.0 intersects with both,
+					// it should show up in both mappings.
+					SignBoundary { min: 1.0, sign: Sign::Negative },
+					SignBoundary { min: 3.0, sign: Sign::Positive },
+				],
+			),
+		];
+		let result = SignBoundary::unions_on(&mapping);
+		assert_eq!(
+			result,
+			vec![
+				SignBoundary { min: 0.0, sign: Sign::Positive },
+				// later normalization will combine...
+				SignBoundary { min: 1.0, sign: Sign::Negative },
+				SignBoundary { min: 1.0, sign: Sign::Negative },
+				SignBoundary { min: 2.0, sign: Sign::Negative },
+				// ...the three negative boundaries
+			]
+		)
+	}
+
+	#[test]
+	fn test_unions_on_rhs_intersects_many() {
+		let rhs = SignBoundary { min: -1.0, sign: Sign::Negative };
+
+		let mapping = vec![
+			(SignBoundary { min: 0.0, sign: Sign::Positive }, vec![rhs.clone()]),
+			(SignBoundary { min: 2.0, sign: Sign::Negative }, vec![rhs.clone()]),
+			(SignBoundary { min: 3.0, sign: Sign::Positive }, vec![rhs.clone()]),
+			(SignBoundary { min: 4.0, sign: Sign::Negative }, vec![rhs.clone()]),
+		];
+		let result = SignBoundary::unions_on(&mapping);
+		assert_eq!(
+			result,
+			vec![
+				// this is unnormalized, but negative on the entire interval from -1.0
+				SignBoundary { min: -1.0, sign: Sign::Negative },
+				SignBoundary { min: -1.0, sign: Sign::Negative },
+				SignBoundary { min: -1.0, sign: Sign::Negative },
+				SignBoundary { min: -1.0, sign: Sign::Negative },
+			]
+		)
+	}
+
+	#[test]
+	fn test_unions_on_rhs_intersects_many_then_flips() {
+		let rhs_start = SignBoundary { min: -1.0, sign: Sign::Negative };
+		let rhs_end = SignBoundary { min: 5.0, sign: Sign::Positive };
+		let mapping = vec![
+			(SignBoundary { min: 0.0, sign: Sign::Positive }, vec![rhs_start.clone()]),
+			(SignBoundary { min: 2.0, sign: Sign::Negative }, vec![rhs_start.clone()]),
+			(SignBoundary { min: 3.0, sign: Sign::Positive }, vec![rhs_start.clone()]),
+			(
+				SignBoundary { min: 4.0, sign: Sign::Negative },
+				vec![rhs_start.clone(), rhs_end.clone()],
+			),
+			(SignBoundary { min: 5.0, sign: Sign::Positive }, vec![rhs_end.clone()]),
+		];
+		let result = SignBoundary::unions_on(&mapping);
+		assert_eq!(
+			result,
+			vec![
+				SignBoundary { min: -1.0, sign: Sign::Negative },
+				SignBoundary { min: -1.0, sign: Sign::Negative },
+				SignBoundary { min: -1.0, sign: Sign::Negative },
+				SignBoundary { min: -1.0, sign: Sign::Negative },
+				SignBoundary { min: 4.0, sign: Sign::Negative },
+				SignBoundary { min: 5.0, sign: Sign::Positive },
 			]
 		)
 	}
