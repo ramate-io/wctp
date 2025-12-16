0344c5eb421fe974eb70eae22929cd5f2c0eca04
Liam Makena Monninger <liam@ramate.io>
2025-12-16 09:17:30 +0100

    docs: indicate how MeshHandle<T> wraps everything up.

--- util/render-item/src/mesh/handle.rs
+++ util/render-item/src/mesh/handle.rs
@@ -1,6 +1,9 @@
-use crate::mesh::{
-	cache::handle::map::HandleMap, cache::handle::MeshHandleCache, cache::mesh::MeshCache,
-	IdentifiedMesh, MeshBuilder, MeshId,
+use crate::{
+	mesh::{
+		cache::handle::map::HandleMap, cache::handle::MeshHandleCache, cache::mesh::MeshCache,
+		IdentifiedMesh, MeshBuilder, MeshId,
+	},
+	NormalizeChunk,
 };
 use bevy::prelude::*;
 use chunk::cascade::CascadeChunk;
@@ -16,18 +19,37 @@ impl<T: MeshBuilder + IdentifiedMesh + Clone> MeshHandle<T> {
 		Self { handle_cache: HandleMap::new(), builder }
 	}
 
+	/// Adds a handle cache to the mesh handle.
 	pub fn with_handle_cache(mut self, handle_cache: HandleMap<T>) -> Self {
 		self.handle_cache = handle_cache;
 		self
 	}
 }
 
+/// We need to implement the identified mesh trait for this to work with the caching and fetcher.
 impl<T: MeshBuilder + IdentifiedMesh + Clone> IdentifiedMesh for MeshHandle<T> {
 	fn id(&self) -> MeshId {
 		self.builder.id()
 	}
 }
 
+/// We need to implement the normalize chunk trait to allow this to work with any of the other traits.
+impl<T: MeshBuilder + IdentifiedMesh + Clone> NormalizeChunk for MeshHandle<T> {
+	fn normalize_chunk(&self, cascade_chunk: &CascadeChunk) -> CascadeChunk {
+		self.builder.normalize_chunk(cascade_chunk)
+	}
+}
+
+/// We can now rederive the mesh builder trait to allow the mesh handle to be used as a mesh builder
+/// which is a requirement for the mesh fetcher.
+impl<T: MeshBuilder + IdentifiedMesh + Clone> MeshBuilder for MeshHandle<T> {
+	fn build_mesh_impl(&self, cascade_chunk: &CascadeChunk) -> Option<Mesh> {
+		self.builder.build_mesh(cascade_chunk)
+	}
+}
+
+/// We implement the mesh cache trait to allow the MeshHandle<T>.
+/// This is the behavior the MeshHandle<T> allows us to wrap in.
 impl<T: MeshBuilder + IdentifiedMesh + Clone> MeshCache for MeshHandle<T> {
 	fn cache_mesh(&self, _mesh: &Mesh, _cascade_chunk: &CascadeChunk) {
 		// do nothing for now
@@ -39,6 +61,8 @@ impl<T: MeshBuilder + IdentifiedMesh + Clone> MeshCache for MeshHandle<T> {
 	}
 }
 
+/// We implement the mesh handle cache trait to allow the MeshHandle<T> to cache the mesh handle.
+/// This is the behavior the MeshHandle<T> allows us to wrap in around a basic builder generically.
 impl<T: MeshBuilder + IdentifiedMesh + Clone> MeshHandleCache for MeshHandle<T> {
 	fn cache_mesh_handle(&self, mesh_handle: Handle<Mesh>, cascade_chunk: &CascadeChunk) {
 		self.handle_cache.insert(cascade_chunk, &self.builder, mesh_handle);
@@ -48,3 +72,5 @@ impl<T: MeshBuilder + IdentifiedMesh + Clone> MeshHandleCache for MeshHandle<T> 
 		self.handle_cache.get(cascade_chunk, &self.builder)
 	}
 }
+
+// We now get the blanket implementation of MeshFetcher for MeshHandle<T>.
