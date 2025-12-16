d1f59549dfadd848f85a29a036ebcc1429cd2c07
Liam Makena Monninger <liam@ramate.io>
2025-12-16 09:06:14 +0100

    fix: eq and hash fro chunk mesh key.

--- util/render-item/src/mesh/cache/handle/map.rs
+++ util/render-item/src/mesh/cache/handle/map.rs
@@ -2,35 +2,49 @@ use crate::mesh::{IdentifiedMesh, MeshId};
 use bevy::prelude::*;
 use chunk::cascade::CascadeChunk;
 use std::collections::HashMap;
-use std::hash::Hash;
+use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 use std::sync::RwLock;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct ChunkMeshKey<T: IdentifiedMesh + Clone> {
+#[derive(Debug, Clone)]
+pub struct ChunkMeshKey<T: IdentifiedMesh> {
 	chunk: CascadeChunk,
 	mesh_id: MeshId,
 	phantom: std::marker::PhantomData<T>,
 }
 
-impl<T: IdentifiedMesh + Clone> ChunkMeshKey<T> {
+impl<T: IdentifiedMesh> PartialEq for ChunkMeshKey<T> {
+	fn eq(&self, other: &Self) -> bool {
+		self.chunk == other.chunk && self.mesh_id == other.mesh_id
+	}
+}
+
+impl<T: IdentifiedMesh> Eq for ChunkMeshKey<T> {}
+
+impl<T: IdentifiedMesh> Hash for ChunkMeshKey<T> {
+	fn hash<H: Hasher>(&self, state: &mut H) {
+		self.chunk.hash(state);
+		self.mesh_id.hash(state);
+		// PhantomData doesn't need to be hashed
+	}
+}
+
+impl<T: IdentifiedMesh> ChunkMeshKey<T> {
 	pub fn new(chunk: CascadeChunk, mesh_id: MeshId) -> Self {
 		Self { chunk, mesh_id, phantom: std::marker::PhantomData }
 	}
 }
 
 #[derive(Debug, Clone)]
-pub struct HandleMap<T: IdentifiedMesh + Clone> {
+pub struct HandleMap<T: IdentifiedMesh> {
 	cache: Arc<RwLock<HashMap<ChunkMeshKey<T>, Handle<Mesh>>>>,
 }
 
-impl<T: IdentifiedMesh + Clone> HandleMap<T> {
+impl<T: IdentifiedMesh> HandleMap<T> {
 	pub fn new() -> Self {
 		Self { cache: Arc::new(RwLock::new(HashMap::new())) }
 	}
-}
 
-impl<T: IdentifiedMesh + Clone> HandleMap<T> {
 	pub fn get(&self, chunk: &CascadeChunk, mesh_builder: &T) -> Option<Handle<Mesh>> {
 		let cache = self.cache.read().unwrap();
 		cache.get(&ChunkMeshKey::new(chunk.clone(), mesh_builder.id())).cloned()
@@ -39,5 +53,11 @@ impl<T: IdentifiedMesh + Clone> HandleMap<T> {
 	pub fn insert(&self, chunk: &CascadeChunk, mesh_builder: &T, mesh: Handle<Mesh>) {
 		let mut cache = self.cache.write().unwrap();
 		cache.insert(ChunkMeshKey::new(chunk.clone(), mesh_builder.id()), mesh);
+	}
+}
+
+impl<T: IdentifiedMesh> Default for HandleMap<T> {
+	fn default() -> Self {
+		Self::new()
 	}
 }
