03830f79a58b35aef153484a9933752368773fa3
Liam Makena Monninger <liam@ramate.io>
2025-12-17 12:41:08 +0100

    debug: playing with tree concepts.

--- playgrounds/objects/src/tree.rs
+++ playgrounds/objects/src/tree.rs
@@ -32,9 +32,16 @@ pub fn tree_playground<M: Material>(mut commands: Commands, material: Res<TreeMa
 	commands.spawn((
 		CascadeChunk::unit_chunk().with_res_2(7),
 		DispatchRenderItem::new(TreeRenderItem::new()),
-		Transform::from_xyz(0.003, 0.003, 0.003)
-			.with_scale(Vec3::new(0.01, 0.01, 0.01))
-			.with_rotation(Quat::from_rotation_arc(Vec3::new(1.0, 1.0, 1.0), Vec3::ZERO)),
+		Transform::from_xyz(0.003, 0.005, 0.004)
+			.with_scale(Vec3::new(0.005, 0.005, 0.005))
+			.with_rotation(Quat::from_rotation_arc(Vec3::new(1.0, 1.0, 1.0).normalize(), Vec3::Y)),
+		MeshMaterial3d(material.0.clone()),
+	));
+
+	commands.spawn((
+		CascadeChunk::unit_chunk().with_res_2(7),
+		DispatchRenderItem::new(TreeRenderItem::new()),
+		Transform::from_xyz(0.0005, 0.0, 0.0005).with_scale(Vec3::new(0.009, 0.02, 0.009)),
 		MeshMaterial3d(material.0.clone()),
 	));
 }
