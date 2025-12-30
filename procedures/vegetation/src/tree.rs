a6127fc17fe7d2904748c38be97cb95d214bf46a
Liam Makena Monninger <liam@ramate.io>
2025-12-29 16:57:36 -0800

    feat: radial branches.

--- procedures/vegetation/src/tree.rs
+++ procedures/vegetation/src/tree.rs
@@ -28,7 +28,7 @@ impl TreeRenderItem {
 
 	pub fn centroid_anchor(&self, transform: Transform) -> Vec3 {
 		let pivot_offset = Vec3::new(0.5, 0.0, 0.5);
-		transform.translation - transform.rotation * (pivot_offset * Vec3::new(0.01, 0.01, 0.01));
+		transform.translation - transform.rotation * (pivot_offset * Vec3::new(0.01, 0.01, 0.01))
 	}
 
 	pub fn spawn_trunk<M: Material>(
@@ -84,16 +84,17 @@ impl TreeRenderItem {
 		let mut branch_builder = BranchBuilder::common_tree_builder();
 
 		// bias the branch towards the top
-		branch_builder.angle_tolerance = 10.0;
-		branch_builder.splitting_coefficient = 0.55;
+		branch_builder.angle_tolerance = 2.0;
+		branch_builder.splitting_coefficient = 0.6;
+		branch_builder.depth = 4;
 
 		// anchor is on the ring of the trunk
-		branch_builder.anchor = self.centroid_anchor(transform);
+		branch_builder.anchor = transform.translation + Vec3::new(0.0, 0.015, 0.0);
 
 		// initial ray is sticking out to the side
 		branch_builder.initial_ray = initial_ray;
-		branch_builder.bias_ray = initial_ray;
-		branch_builder.bias_amount = 0.2;
+		branch_builder.bias_ray = initial_ray + Vec3::new(0.0, 0.01, 0.0);
+		branch_builder.bias_amount = 0.5;
 
 		// min segment length is 0.002
 		branch_builder.min_segment_length = 0.002;
@@ -107,6 +108,8 @@ impl TreeRenderItem {
 		// max radius is 0.004
 		branch_builder.max_radius = 0.002;
 
+		branch_builder.initial_radius = branch_builder.max_radius;
+
 		let branch = branch_builder.build();
 
 		// for now use the trunk segment
@@ -114,13 +117,10 @@ impl TreeRenderItem {
 		let mesh_handle = MeshHandle::new(tree_segment).with_handle_cache(self.tree_cache.clone());
 
 		for segment in branch.segments() {
+			log::info!("Segment: {:?}", segment);
 			let ray = segment.ray();
 			let direction = ray.clone().normalize();
 			let length = ray.length();
-
-			log::info!("Segment: {:?}, Direction: {:?}, Length: {:?}", segment, direction, length);
-
-			// let rotation = Quat::from_rotation_arc(Vec3::Y, direction);
 
 			let up = direction;
 
@@ -141,6 +141,8 @@ impl TreeRenderItem {
 				scale,
 			};
 
+			log::info!("Transform: {:?}", transform);
+
 			commands.spawn((
 				cascade_chunk.clone(),
 				MeshDispatch::new(mesh_handle.clone()),
@@ -159,10 +161,10 @@ impl TreeRenderItem {
 		branch_count: usize,
 	) {
 		for i in 0..branch_count {
-			let initial_ray = Vec3::new(0.0, 1.0, 1.0).with_rotation(Quat::from_rotation_y(
-				i as f32 * 2.0 * std::f32::consts::PI / branch_count as f32,
-			));
-			self.spawn_branch(commands, cascade_chunk, transform, material, initial_ray);
+			let angle = i as f32 * 2.0 * std::f32::consts::PI / branch_count as f32;
+			let initial_ray =
+				Vec3::new(angle.cos(), angle.sin() + angle.cos(), angle.sin()).normalize();
+			self.spawn_branch(commands, cascade_chunk, transform, material.clone(), initial_ray);
 		}
 	}
 }
