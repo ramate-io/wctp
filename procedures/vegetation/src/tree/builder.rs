6f44bc8e31b5bae74cdf6081a65234ed7c8a04c5
Liam Makena Monninger <liam@ramate.io>
2026-01-05 20:29:55 -0800

    chore: structuring tree.

--- procedures/vegetation/src/tree/builder.rs
+++ procedures/vegetation/src/tree/builder.rs
@@ -1,12 +1,27 @@
 use bevy::prelude::*;
 use comproc::{complex::chain::ball_stick::builder::BallStick, noise::config::NoiseConfig};
 use noise::{NoiseFn, Seedable};
+use render_item::{
+	mesh::{IdentifiedMesh, MeshBuilder},
+	NormalizeChunk,
+};
 use std::fmt::Debug;
 
-pub struct Tree {
+pub struct Tree<
+	StickMesh: MeshBuilder + NormalizeChunk + IdentifiedMesh,
+	TrunkMesh: MeshBuilder + NormalizeChunk + IdentifiedMesh,
+	LeafMesh: MeshBuilder + NormalizeChunk + IdentifiedMesh,
+	TrunkMaterial: Material,
+	LeafMaterial: Material,
+> {
 	anchor: Vec3,
 	height: f32,
+	trunk_material: MeshMaterial3d<T>,
+	leaf_material: MeshMaterial3d<L>,
 	branch_ball_sticks: Vec<BallStick>,
+	leaf_ball_scale: Vec3,
+	trunk_cache: HandleMap<SimpleTrunkSegment>,
+	leaf_cache: HandleMap<NoisyBall>,
 }
 
 pub struct TreeBuilder<N: NoiseFn<f64, 3> + Seedable + Debug + Clone> {
