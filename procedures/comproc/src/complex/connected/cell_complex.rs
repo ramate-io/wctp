088a02a78fbece5e3d43112c9b7bea6659919a16
Liam Makena Monninger <liam@ramate.io>
2026-01-07 22:46:52 -0800

    feat: cell complex builder.

--- procedures/comproc/src/complex/connected/cell_complex.rs
+++ procedures/comproc/src/complex/connected/cell_complex.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::*;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
-use std::hash::{Hash, Hasher};
+use std::hash::Hash;
+use std::marker::PhantomData;
 
 pub trait Vertex: Hash + Eq + Clone + Debug {
 	fn point(&self) -> Vec3;
@@ -13,8 +14,45 @@ pub trait Face<O, V: Vertex, E: Edge<O, V>>: Hash + Eq + Clone + Debug {
 	fn edges(&self, on: &O) -> Vec<E>;
 }
 
-pub struct CellComplex3d<V, E, F> {
-	pub v_to_e: HashMap<V, E>,
-	pub e_to_f: HashMap<E, F>,
-	pub f_to_v: HashMap<F, V>,
+pub struct CellComplex3d<O, V: Vertex, E: Edge<O, V>, F: Face<O, V, E>> {
+	pub v_to_e: HashMap<V, HashSet<E>>,
+	pub e_to_f: HashMap<E, HashSet<F>>,
+	__phantom_o: PhantomData<O>,
+}
+
+impl<O, V: Vertex, E: Edge<O, V>, F: Face<O, V, E>> CellComplex3d<O, V, E, F> {
+	pub fn new() -> Self {
+		Self { v_to_e: HashMap::new(), e_to_f: HashMap::new(), __phantom_o: PhantomData }
+	}
+
+	fn add_edge(&mut self, edge: E, on: &O) {
+		for vertex in edge.vertices(on) {
+			self.v_to_e.entry(vertex).or_insert(HashSet::new()).insert(edge.clone());
+		}
+	}
+
+	/// Adds a face to the cell complex, adds edges to the cell complex as well.
+	pub fn add_face(&mut self, face: F, on: &O) {
+		for edge in face.edges(on) {
+			self.e_to_f.entry(edge.clone()).or_insert(HashSet::new()).insert(face.clone());
+			self.add_edge(edge, on);
+		}
+	}
+
+	pub fn unique_faces(&self) -> HashSet<&F> {
+		self.e_to_f.values().flat_map(|e| e.iter()).collect()
+	}
+
+	pub fn build(&mut self, builder: &mut impl CellComplex3dBuilder<O, V, E, F>, on: &O) {
+		let mut last_face = None;
+		while let Some(face) = builder.next_face(self, last_face) {
+			self.add_face(face.clone(), on);
+			last_face = Some(face);
+		}
+	}
+}
+
+pub trait CellComplex3dBuilder<O, V: Vertex, E: Edge<O, V>, F: Face<O, V, E>> {
+	fn next_face(&mut self, complex: &CellComplex3d<O, V, E, F>, last_face: Option<F>)
+		-> Option<F>;
 }
